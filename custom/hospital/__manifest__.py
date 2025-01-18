d7f0c7be20082c19d4b8ea941d85b61b12d13a58
emon273273 <emon.cse273@gmail.com>
2025-01-18 23:12:56 +0600

    created two custom module in hospital

---
+++ custom/hospital/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name":"hospital",
+    "author":"emon",
+    "version":"1.0",
+    "catagory":"Custom",
+    "license":"LGPL-3",
+    "data":[
+        'security/ir.model.access.csv',
+        'views/patient.xml',
+        'views/doctor.xml',
+        'views/menu.xml'
+    ],
+     'installable': True,
+    'application': True,  # Makes it appear in the Apps menu
+    'auto_install': False,
+}
