9f4bf31b5d33fcb63cc238eea10cb02e4069721a
emon273273 <emon.cse273@gmail.com>
2025-01-26 11:29:15 +0600

    jan 26

--- custom/hospital/__manifest__.py
+++ custom/hospital/__manifest__.py
@@ -1,28 +1,21 @@
 {
-    "name":"hospital",
-    "author":"emon",
-    "version":"1.0",
-    "catagory":"Custom",
-    "license":"LGPL-3",
-<<<<<<< HEAD
-    'depends': ['base', 'hr', 'product','account'],
-    "data":[
-        'views/pharmacy_views.xml',
-        'security/ir.model.access.csv',
-        'data/sequence_data.xml',
-        'views/account_move_view.xml',
-        'views/doctor.xml',
-        'views/appointment_views.xml',
-        'views/patient_registation.xml',
-=======
-    "data":[
-        'security/ir.model.access.csv',
-        'views/patient.xml',
-        'views/doctor.xml',
->>>>>>> d7f0c7be20082c19d4b8ea941d85b61b12d13a58
-        'views/menu.xml'
+    "name": "hospital",
+    "author": "emon",
+    "version": "1.0",
+    "category": "Custom",  # Ensure "category" is spelled correctly
+    "license": "LGPL-3",
+    "depends": ["base", "hr", "product", "account"],
+    "data": [
+        "views/pharmacy_views.xml",
+        "security/ir.model.access.csv",
+        "data/sequence_data.xml",
+        "views/account_move_view.xml",
+        "views/doctor.xml",
+        "views/appointment_views.xml",
+        "views/patient_registation.xml",
+        "views/menu.xml"
     ],
-     'installable': True,
-    'application': True,  # Makes it appear in the Apps menu
-    'auto_install': False,
+    "installable": True,  # Ensure this is set to True
+    "application": True,  # Makes it appear in the Apps menu
+    "auto_install": False,
 }
--- custom/hospital/models/patient.py
+++ custom/hospital/models/patient.py
@@ -3,19 +3,13 @@ from odoo import api, fields, models
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "This is just for testing purposes"
-<<<<<<< HEAD
     patient_id=fields.Char(string="Patient ID",readonly=True)
     name = fields.Char(string="Name", required=True)
     age=fields.Integer(string="Age",required=True)
-=======
-
-    name = fields.Char(string="Name", required=True)
->>>>>>> d7f0c7be20082c19d4b8ea941d85b61b12d13a58
     date_of_birth = fields.Date(string="Date of Birth")
     gender = fields.Selection(
         [('male', "Male"), ('female', "Female")],
         string="Gender",
-<<<<<<< HEAD
         required=True
     )
     contact_number = fields.Char(string='Contact Number')
@@ -26,8 +20,3 @@ class HospitalPatient(models.Model):
     def create(self, vals):
         vals['patient_id'] = self.env['ir.sequence'].next_by_code('hospital.patient') 
         return super(HospitalPatient, self).create(vals) 
-=======
-        required=False
-    )
-    
->>>>>>> d7f0c7be20082c19d4b8ea941d85b61b12d13a58
